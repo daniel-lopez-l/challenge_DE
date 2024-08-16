106f9c92f248c5c67dbc4b07f8a376bfaf885224
Daniel Lopez (THOUGHTWORKS) <daniellopez.thoughtworks@latam.com>
2024-08-16 17:54:18 -0400

    add q1_time and q1_memory WIP from colab

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,18 +1,16 @@
 from typing import List, Tuple
-from datetime import datetime
 from google.cloud import bigquery
 
-def q1_memory(client: bigquery.Client(), query:str) -> List[Tuple[str, str]]:
-    # Ejecutar la consulta
+from memory_profiler import profile
+
+@profile
+def q1_memory(client: bigquery.Client, query: str) ->  List[Tuple[str, str]]:
+
     query_job = client.query(query)
-
-
     query_job.result()
+
     results = query_job.result()
+
     output = [(row.top_user, row.date) for row in results]
 
-   
-    # print("QUERY 1")
-    # print ("Las top 10 fechas donde hay más tweets. Ordenadas por usuario (username) que más publicaciones tiene por cada uno de esos días: ")
-    # print(output)
     return output
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,29 @@
 from typing import List, Tuple
-from datetime import datetime
+import cProfile
+import pstats
+import time
+from google.cloud import bigquery
 
-def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+
+def q1_time(client: bigquery.Client, query: str) -> Tuple[List[Tuple[str, str]], float]:
+    profiler = cProfile.Profile()
+    profiler.enable()
+    
+    start_time = time.time()
+    
+    query_job = client.query(query)
+    query_job.result()
+    results = query_job.result()
+    
+    output = [(row.top_user, row.date) for row in results]
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    profiler.disable()
+    
+    stats = pstats.Stats(profiler)
+    stats.strip_dirs().sort_stats('cumtime').print_stats(10)  # Mostrar las 10 líneas más lentas
+
+    print(f"Tiempo total de ejecución: {execution_time:.2f} segundos")
+    
+    return output, execution_time
