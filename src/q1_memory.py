3381192c8769f9d8bac9b9e217268b7297d20bdc
Daniel Lopez (THOUGHTWORKS) <daniellopez.thoughtworks@latam.com>
2024-08-17 18:39:01 -0400

    add memory_utils and update q1,q2,q3 memory files

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,15 +1,12 @@
 from typing import List, Tuple
 from google.cloud import bigquery
-
 from memory_profiler import profile
+from memory_utils import execute_query_for_memory
 
 @profile
 def q1_memory(client: bigquery.Client, query: str) ->  List[Tuple[str, str]]:
 
-    query_job = client.query(query)
-    query_job.result()
-
-    results = query_job.result()
+    results = execute_query_for_memory(client, query)
 
     output = [(row.top_user, row.date) for row in results]
 
--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,13 @@
 from typing import List, Tuple
+from google.cloud import bigquery
+from memory_profiler import profile
+from memory_utils import execute_query_for_memory
 
-def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+@profile
+def q2_memory(client: bigquery.Client, query: str) ->  List[Tuple[str, int]]:
+
+    results = execute_query_for_memory(client, query)
+
+    output = [(row.emoji, row.total_emoji) for row in results]
+
+    return output
