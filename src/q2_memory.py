b6761dbf0be4c0acab472061d7354813a931c0e8
Daniel Lopez (THOUGHTWORKS) <daniellopez.thoughtworks@latam.com>
2024-08-17 19:24:48 -0400

    Refactor: multiple files

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -5,7 +5,14 @@ from memory_utils import execute_query_for_memory
 
 @profile
 def q2_memory(client: bigquery.Client, query: str) ->  List[Tuple[str, int]]:
-
+    """
+    Execute and return the result of query and the memory
+    Args:
+        client (bigquery.Client),
+        query (str)
+    Returns: 
+        result of query
+    """
     results = execute_query_for_memory(client, query)
 
     output = [(row.emoji, row.total_emoji) for row in results]
--- src/q3_memory.py
+++ src/q3_memory.py
@@ -5,7 +5,14 @@ from memory_utils import execute_query_for_memory
 
 @profile
 def q3_memory(client: bigquery.Client, query: str) ->  List[Tuple[str, int]]:
-
+    """
+    Execute and return the result of query and the memory
+    Args:
+        client (bigquery.Client),
+        query (str)
+    Returns: 
+        result of query
+    """
     results = execute_query_for_memory(client, query)
 
     output = [(row.top_mentioned, row.total_mencioned) for row in results]
