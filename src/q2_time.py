f176e08ba60d6e3ac7dfaf8aa0e87fa53ab822b8
Daniel López <111003199+daniel-lopez-l@users.noreply.github.com>
2024-08-17 19:49:11 -0400

    Revert "Feature to Prod"

--- src/q2_time.py
+++ src/q2_time.py
@@ -4,14 +4,16 @@ from google.cloud import bigquery
 
 
 def q2_time(client: bigquery.Client, query: str) -> Tuple[List[Tuple[str, int]], float]:
+   
     """
-    Execute exetute_time_query functions for return the result of query and time
+    Execute and return the result of query and the time in ms
     Args:
         client (bigquery.Client),
         query (str)
     Returns: 
         result of query and the time in ms
     """
+    
     q2_query_result, q2_time_result  = execute_time_query(client ,query)
     output = [(row.emoji, row.total_emoji) for row in q2_query_result]
     return output, q2_time_result
