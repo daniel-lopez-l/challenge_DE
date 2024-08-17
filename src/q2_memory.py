a9cde967bf7f5be213c391b2c0a442a31c5d46da
Daniel López <111003199+daniel-lopez-l@users.noreply.github.com>
2024-08-17 18:40:17 -0400

    Revert "add memory_utils and update q1,q2,q3 memory files"

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,13 +1,4 @@
 from typing import List, Tuple
-from google.cloud import bigquery
-from memory_profiler import profile
-from memory_utils import execute_query_for_memory
 
-@profile
-def q2_memory(client: bigquery.Client, query: str) ->  List[Tuple[str, int]]:
-
-    results = execute_query_for_memory(client, query)
-
-    output = [(row.emoji, row.total_emoji) for row in results]
-
-    return output
+def q2_memory(file_path: str) -> List[Tuple[str, int]]:
+    pass
--- src/q3_memory.py
+++ src/q3_memory.py
@@ -1,13 +1,4 @@
 from typing import List, Tuple
-from google.cloud import bigquery
-from memory_profiler import profile
-from memory_utils import execute_query_for_memory
 
-@profile
-def q3_memory(client: bigquery.Client, query: str) ->  List[Tuple[str, int]]:
-
-    results = execute_query_for_memory(client, query)
-
-    output = [(row.top_mentioned, row.total_mencioned) for row in results]
-
-    return output
+def q3_memory(file_path: str) -> List[Tuple[str, int]]:
+    pass
