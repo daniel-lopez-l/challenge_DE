6ec78e3941b9f3bd30bb7b06ec2ef4b3790772a7
Daniel Lopez (THOUGHTWORKS) <daniellopez.thoughtworks@latam.com>
2024-08-17 20:02:36 -0400

    chore: change import in q1 memory

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -3,6 +3,8 @@ from google.cloud import bigquery
 from memory_profiler import profile
 from memory_utils import execute_query_for_memory
 
+
+
 @profile
 def q1_memory(client: bigquery.Client, query: str) ->  List[Tuple[str, str]]:
     """
