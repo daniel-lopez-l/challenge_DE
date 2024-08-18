8668655565e460e575f1276a4611a9e2b73bd053
Daniel Lopez (THOUGHTWORKS) <daniellopez.thoughtworks@latam.com>
2024-08-17 21:47:38 -0400

    "add notebook colab and memory utils again"

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -3,12 +3,8 @@ from google.cloud import bigquery
 from memory_profiler import profile
 from memory_utils import execute_query_for_memory
 
-
-
-
-
 @profile
-def q1_memory(client: bigquery.Client, query: str) ->  List[Tuple[str, str]]:
+def q1_memory(client: bigquery.Client, query: str) -> List[Tuple[str, str]]:
     """
     Execute and return the result of query and the memory
     Args:
